df54ae12c3c73c5e013544dade5975ba64194353
pqt46 <pqt4621@gmail.com>
2023-04-13 22:59:45 +0700

    add UI Back Dialog

--- app/src/main/java/com/example/qtor/ui/editor/EditorActivity.kt
+++ app/src/main/java/com/example/qtor/ui/editor/EditorActivity.kt
@@ -3,23 +3,22 @@ package com.example.qtor.ui.editor
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.addCallback
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.example.qtor.R
 import com.example.qtor.constant.IMAGE_TO_EDIT
-import com.example.qtor.constant.LOADING
 import com.example.qtor.constant.TOOL_INIT_INDEX
 import com.example.qtor.ui.editor.ui.theme.QTorTheme
 import org.opencv.android.OpenCVLoader
 
 class EditorActivity : ComponentActivity() {
     private val viewModel by viewModels<EditorViewModel>()
+    private var showDialog = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         OpenCVLoader.initDebug()
@@ -29,9 +28,18 @@ class EditorActivity : ComponentActivity() {
         intent.getIntExtra(TOOL_INIT_INDEX,0).let {
             viewModel.setMainToolActive(it)
         }
+        onBackPressedDispatcher.addCallback {
+            showDialog=true
+        }
         setContent {
             QTorTheme {
                 EditorTheme(viewModel = viewModel, context = this)
+                Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxSize().clickable {  }) {
+                    if (showDialog){
+                        CustomDialogUI()
+                    }
+                }
+
             }
         }
     }
--- app/src/main/java/com/example/qtor/ui/editor/EditorCompose.kt
+++ app/src/main/java/com/example/qtor/ui/editor/EditorCompose.kt
@@ -1,23 +1,29 @@
 package com.example.qtor.ui.editor
 
 import android.content.Context
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.example.qtor.R
 import com.example.qtor.constant.LOADING
@@ -63,15 +69,18 @@ fun EditorTheme(viewModel: EditorViewModel, context: Context) {
             )
 
         }
-        Column(Modifier.fillMaxSize(),
+        Column(
+            Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Bottom
 
         ) {
-            if (mainToolActive== MAIN_TOOl_REMOVE_OBJECT){
-                Row(horizontalArrangement = Arrangement.Start , modifier = Modifier
-                    .padding(horizontal = 10.dp)
-                    .fillMaxWidth()) {
+            if (mainToolActive == MAIN_TOOl_REMOVE_OBJECT) {
+                Row(
+                    horizontalArrangement = Arrangement.Start, modifier = Modifier
+                        .padding(horizontal = 10.dp)
+                        .fillMaxWidth()
+                ) {
                     CircleButton(image = ImageVector.vectorResource(id = R.drawable.ic_undo)) {
                         viewModel.undo()
                     }
@@ -86,10 +95,95 @@ fun EditorTheme(viewModel: EditorViewModel, context: Context) {
                     .weight(2f), viewModel = viewModel
             )
         }
-        if (!state){
+        if (!state) {
             Column(modifier = Modifier
                 .fillMaxSize()
                 .clickable { }) {}
+        }
+    }
+}
+
+
+@Composable
+fun CustomDialogUI(modifier: Modifier = Modifier) {
+    Card(
+        //shape = MaterialTheme.shapes.medium,
+        shape = RoundedCornerShape(10.dp),
+        // modifier = modifier.size(280.dp, 240.dp)
+        modifier = Modifier.padding(10.dp, 5.dp, 10.dp, 10.dp),
+        elevation = 8.dp
+    ) {
+        Column(
+            modifier
+                .background(Color.White)
+        ) {
+
+            //.......................................................................
+            Image(
+                painter = painterResource(id = R.drawable.ic_notifi),
+                contentDescription = null, // decorative
+                contentScale = ContentScale.Fit,
+                colorFilter = ColorFilter.tint(
+                    color = MaterialTheme.colors.primary
+                ),
+                modifier = Modifier
+                    .padding(top = 35.dp)
+                    .height(70.dp)
+                    .fillMaxWidth(),
+
+                )
+
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = stringResource(id = R.string.discard_change),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(top = 5.dp)
+                        .fillMaxWidth(),
+                    style = MaterialTheme.typography.h6,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = stringResource(id = R.string.alert_discard_change),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(top = 10.dp, start = 25.dp, end = 25.dp)
+                        .fillMaxWidth(),
+                    style = MaterialTheme.typography.body2
+                )
+            }
+            //.......................................................................
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp)
+                    .background(MaterialTheme.colors.background),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+
+                TextButton(onClick = {
+//                    openDialogCustom.value = false
+                }) {
+
+                    Text(
+                        text = stringResource(id = R.string.cancel),
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colors.primary,
+                        modifier = Modifier.padding(top = 5.dp, bottom = 5.dp)
+                    )
+                }
+                TextButton(onClick = {
+//                    openDialogCustom.value = false
+                }) {
+                    Text(
+                        stringResource(id = R.string.confirm),
+                        fontWeight = FontWeight.ExtraBold,
+                        color = Color.Black,
+                        modifier = Modifier.padding(top = 5.dp, bottom = 5.dp)
+                    )
+                }
+            }
         }
     }
 }
